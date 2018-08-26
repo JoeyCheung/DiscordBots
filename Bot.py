b85fbc1de520038581e0fbe7217fa1532cd5d30a
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2018-08-26 00:31:28 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -14,17 +14,42 @@ async def on_ready():
 
 
 @bot.command(pass_context=True)
-async def info(ctx,user: discord.Member):
-    await bot.say("The users name is: {}".format(user.name))
-    await bot.say("The users ID is: {}".format(user.id))
-    await bot.say("The users status is: {}".format(user.top_role))
-    await bot.say("The user joined at: {}".format(user.joined_at))
+async def info(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
 
 
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Here's what I could find.", color=0x00ff00)
+    embed.set_author(name="QC Smash Club")
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)
+    
+    
 @bot.command(pass_context=True)
 async def kick(ctx, user: discord.Member):
     await bot.say(":boot: Your test is too low, {}".format(user.name))
     await bot.kick(user)
 
 
+@bot.command(pass_context=True)
+async def embed(ctx):
+    embed = discord.Embed(title="test", description="my name jeff", color=0x00ff00)
+    embed.set_footer(text="this is a footer")
+    embed.set_author(name="Will Ryan of DAGames")
+    embed.add_field(name="This is a field", value="no it isn't", inline=True)
+    await bot.say(embed=embed)
+    
+    
 bot.run("")
